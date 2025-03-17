e0e43deb68db247c735bb86872a7011d3f6c8cf2
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-17 17:05:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "kubernetes_namespace" "monitoring" {
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
-  chart      = "prometheus-community/prometheus"
+  chart      = "prometheus-community/kube-prometheus-stack"  # Correct chart name
   repository = "https://prometheus-community.github.io/helm-charts"
   values     = []
 }
@@ -15,9 +15,10 @@ resource "helm_release" "prometheus" {
 resource "helm_release" "grafana" {
   name       = "grafana"
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
-  chart      = "grafana/grafana"
+  chart      = "grafana/grafana"  # Correct chart name
   repository = "https://grafana.github.io/helm-charts"
-  
+  values     = []
+}  
   set {
     name  = "service.type"
     value = "LoadBalancer"
