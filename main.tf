079863c10142ca2f9ecdb3f431a7416e6e5a6a4f
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-17 17:17:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "helm_release" "prometheus" {
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
   chart      = "prometheus-community/kube-prometheus-stack"
   repository = "https://prometheus-community.github.io/helm-charts"
+  version    = "70.0.2"  # Update this to the correct version
 }
 
 resource "helm_release" "grafana" {
@@ -16,6 +17,7 @@ resource "helm_release" "grafana" {
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
   chart      = "grafana/grafana"
   repository = "https://grafana.github.io/helm-charts"
+  version    = "6.56.2"  # Update this to the correct version
 
   set {
     name  = "service.type"
@@ -27,7 +29,6 @@ resource "helm_release" "grafana" {
     value = "3000"
   }
 }
-
 output "grafana_url" {
   description = "The external IP address of the Grafana dashboard"
   value       = "http://${helm_release.grafana.name}.monitoring.svc.cluster.local:3000"
