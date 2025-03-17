f8a74140e3edfb06f29f6b19da0a4605d629b433
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-17 18:34:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,17 +8,17 @@ resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  version    = "70.0.2"  # Ensure this matches a valid version
-  namespace  = "monitoring"
+  version    = "18.0.13"  # Latest available version from the provided list
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
 }
 
 resource "helm_release" "grafana" {
   name       = "grafana"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
-  version    = "8.10.4"  # Replace with a valid Grafana chart version
-  namespace  = "monitoring"
-}
+  version    = "8.10.4"  # Ensure this is a valid Grafana chart version
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+
   set {
     name  = "service.type"
     value = "LoadBalancer"
@@ -29,7 +29,8 @@ resource "helm_release" "grafana" {
     value = "3000"
   }
 }
+
 output "grafana_url" {
   description = "The external IP address of the Grafana dashboard"
-  value       = "http://${helm_release.grafana.name}.monitoring.svc.cluster.local:3000"
+  value       = "http://${helm_release.grafana.status.0.load_balancer.0.ingress.0.ip}:3000"
 }
